96cc378e17ff806f1d1ec1afd1020892ed2ffd5e
Itsmesmita <157411561+Itsmesmita@users.noreply.github.com>
2024-01-22 11:40:01 -0800

    try

---
+++ grade.py
@@ -0,0 +1,9 @@
+score = int (input('What is your score: '))
+if 100>=score>=90:
+    print('Grade AA')
+elif 89>=score>=80:
+    print ('Grade A')
+elif 79<=score<=50:
+    print ('Grade B')
+else:
+    print('Fail')
